646f8c4ee4e7c8e8e7bcf81fd2fa27eac2ee72a8
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-08-10 20:45:22 +0200

    Show on UI thread.

--- ActivityView.m
+++ ActivityView.m
@@ -13,7 +13,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
     NSString *imageUrl = args[@"imageUrl"];
     NSString *image = args[@"image"];
     NSData * imageData;
-    
+
     // Try to fetch image
     if (imageUrl) {
         @try {
@@ -22,33 +22,35 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)args)
             RCTLogWarn(@"Could not fetch image.");
         }
     }
-    
-    
+
+
     // Return if no args were passed
     if (!text && !url && !image && !imageData) {
         RCTLogError(@"[ActivityView] You must specify a text, url, image and/or imageUrl.");
         return;
     }
-    
+
     if (text) {
         [shareObject addObject:text];
     }
-    
+
     if (url) {
         [shareObject addObject:url];
     }
-    
+
     if (image) {
         [shareObject addObject: [UIImage imageNamed: image]];
     } else if (imageData) {
         [shareObject addObject: [UIImage imageWithData: imageData]];
     }
-    
-    UIActivityViewController *activityView = [[UIActivityViewController alloc] initWithActivityItems:shareObject applicationActivities:nil];
-    
-    // Display the Activity View
-    UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    [ctrl presentViewController:activityView animated:YES completion:nil];
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIActivityViewController *activityView = [[UIActivityViewController alloc] initWithActivityItems:shareObject applicationActivities:nil];
+
+        // Display the Activity View
+        UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        [ctrl presentViewController:activityView animated:YES completion:nil];
+    });
 }
 
 @end
